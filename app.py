5868a17f4c05e892120b1d34c8037fe4ef06773a
Danish Hussain <hussaindenish@gmail.com>
2025-07-06 13:41:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from Jenkins + Ansible CI/CD on Localhost!"
+def hello():
+    return "Hello from Jenkins + Ansible + Flask CI/CD!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0')
